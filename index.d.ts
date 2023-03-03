71ec033377ff27c34b6621c72c5de1bbcf39da66
knightcat.sq@gmail.com <knightcat.sq@gmail.com>
2023-03-03 18:35:12 +0800

    Remove old files.

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="./lib/math.d.ts" />
-/// <reference path="./lib/utils.d.ts" />
-/// <reference path="./lib/token.d.ts" />
-/// <reference path="./lib/syntax.d.ts" />
-/// <reference path="./lib/parser.d.ts" />
-/// <reference path="./lib/evaluator.d.ts" />
